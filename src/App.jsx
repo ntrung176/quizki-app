ec41a0115d20b29d0b904c4994db331b82117d99
ntrung176 <lynguyennhattrung1706@gmail.com>
2025-12-15 22:21:24 +0700

    change api

--- src/App.jsx
+++ src/App.jsx
@@ -298,7 +298,7 @@ const playBrowserTts = (text) => {
 
 const _fetchTtsApiCall = async (text, voiceName, attempts = 0) => {
     if (!text) return null;
-    const apiKey = "AIzaSyB4KYtt-aeIAehw1qWTY3XKThWeakmcmFY"; 
+    const apiKey = "AIzaSyDsrpaJm89kB_3KCrYLBI3pJlc9VSSTh-k"; 
     const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-tts:generateContent?key=${apiKey}`;
     
     const payload = {
